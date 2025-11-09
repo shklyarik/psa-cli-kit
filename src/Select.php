a98de385f0043b1d6fcf3110c7f30e7b23296ef6
Pavel <dementorpasha@gmail.com>
2025-11-09 11:13:35 +0300

    Improve UX of Select

--- src/Select.php
+++ src/Select.php
@@ -6,9 +6,30 @@ class Select
 {
     public function __construct(
         private array $options,
-        private string $message = 'Select a option:',
+        private string $message = 'Select an option:',
     )
     {
+    }
+
+    private function displayOptions($title, $options, $currentChoice, $previousLines = 0)
+    {
+        // Поднимаемся на количество строк, чтобы перерисовать выбор
+        if ($previousLines > 0) {
+            echo "\033[" . $previousLines . "A";
+        }
+
+        $output = "$title\n--------------------------\n";
+        $index = 0;
+        foreach ($options as $value) {
+            if ($index === $currentChoice) {
+                $output .= "👉 $value\n";
+            } else {
+                $output .= "   $value\n";
+            }
+            $index++;
+        }
+
+        echo $output;
     }
 
     public function index()
@@ -17,23 +38,10 @@ class Select
         $optionKeys = array_keys($this->options);
         $maxIndex = count($this->options) - 1;
 
-        function displayOptions($title, $options, $currentChoice) {
-            system('clear');
-            echo "$title\n--------------------------\n";
-            $index = 0;
-            foreach ($options as $key => $value) {
-                if ($index === $currentChoice) {
-                    echo "👉 $value\n";
-                } else {
-                    echo "   $value\n";
-                }
-                $index++;
-            }
-        }
-
         system('stty -icanon -echo');
 
-        displayOptions($this->message, $this->options, $currentChoice);
+        $this->displayOptions($this->message, $this->options, $currentChoice);
+        $totalLines = count($this->options) + 2; // message + divider + options
 
         while (true) {
             $char = fread(STDIN, 3);
@@ -45,10 +53,15 @@ class Select
                 break;
             }
 
-            displayOptions($this->message, $this->options, $currentChoice);
+            $this->displayOptions($this->message, $this->options, $currentChoice, $totalLines);
         }
 
         system('stty sane');
+
+        // Стираем предыдущие строки и выводим результат
+        echo "\033[" . $totalLines . "A"; // подняться
+        echo "\033[0J"; // очистить всё ниже
+        echo "{$this->message} : {$this->options[$currentChoice]}\n";
 
         return $optionKeys[$currentChoice];
     }
